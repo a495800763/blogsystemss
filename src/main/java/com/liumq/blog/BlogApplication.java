e3f0e8279e34fc0e24861b2f6ef5ec5101188225
a495800763 <495800763@qq.com>
2020-11-12 21:12:19 +0800

    first commit

---
+++ src/main/java/com/liumq/blog/BlogApplication.java
@@ -0,0 +1,14 @@
+package com.liumq.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        //一个小而美的个人博客
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
