862c612a3d23eb5f8656dca978d6ba3abec3ce9a
a495800763 <495800763@qq.com>
2020-12-06 22:17:19 +0800

    springboot配置相关

--- src/main/java/com/liumq/blog/BlogApplication.java
+++ src/main/java/com/liumq/blog/BlogApplication.java
@@ -3,13 +3,14 @@ package com.liumq.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.Console;
+
 @SpringBootApplication
 public class BlogApplication {
 
     public static void main(String[] args) {
         //一个小而美的个人博客
-        SpringApplication.run(
-                BlogApplication.class, args);
+        SpringApplication.run(BlogApplication.class, args);
     }
 
 }
