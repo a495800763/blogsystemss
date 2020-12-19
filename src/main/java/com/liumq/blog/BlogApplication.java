83ce9f2cfdc701571dc0e8a38e46c24bdedf8a34
a495800763 <495800763@qq.com>
2020-12-19 17:32:59 +0800

    插件集成0.2

--- src/main/java/com/liumq/blog/BlogApplication.java
+++ src/main/java/com/liumq/blog/BlogApplication.java
@@ -3,8 +3,6 @@ package com.liumq.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.Console;
-
 @SpringBootApplication
 public class BlogApplication {
 
