1942b9332c66185baa97644e7f2c5f6513cd06e6
liumq <495800763@qq.com>
2021-05-05 22:42:37 +0800

    异常处理

---
+++ src/main/java/com/liumq/blog/handler/ControllerExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.liumq.blog.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHandler(HttpServletRequest request, Exception e) throws Exception {
+        logger.error("Request URL : {}, Exception: {}", request.getRequestURL(), e);
+        //如果抛出的异常类是打了ResponseStatus注解的类（自定义异常类）则继续抛出错误
+        //使得ResponseStatus注解生效返回到指定的异常页面，而不是error页面
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("url", request.getRequestURL());
+        mv.addObject("exception", e);
+        mv.setViewName("error/error");
+        return mv;
+    }
+}
